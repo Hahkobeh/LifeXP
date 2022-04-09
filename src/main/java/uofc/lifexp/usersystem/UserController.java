f6ed33d036e399b960b6a274f20bcdd08d9b63cb
Jacob <hahkobeh@gmail.com>
2022-04-09 16:58:06 -0600

    user system

--- src/main/java/uofc/lifexp/usersystem/UserController.java
+++ src/main/java/uofc/lifexp/usersystem/UserController.java
@@ -2,10 +2,13 @@ package uofc.lifexp.usersystem;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import uofc.lifexp.usersystem.friend.FriendService;
+import uofc.lifexp.usersystem.user.User;
+import uofc.lifexp.usersystem.user.UserForm;
 import uofc.lifexp.usersystem.user.UserService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "api/user")
@@ -18,5 +21,27 @@ public class UserController {
         this.friendService = friendService;
         this.userService = userService;
     }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public User login(@RequestBody UserForm userForm){
+        System.out.println("Login request received");
+        return userService.login(userForm.getUsername(), userForm.getPassword());
+    }
+
+    @PostMapping("/register")
+    public boolean register(@RequestBody UserForm userForm){
+        System.out.println("Register request received");
+        return userService.createUser(userForm.getUsername(),userForm.getPassword());
+    }
+
+    @GetMapping("/get-friends/{username}")
+    @ResponseBody
+    public List<String> getFriends(@PathVariable String username){
+        System.out.println("Get friends request received");
+        return friendService.getFriends(username);
+    }
+
+
 
 }
