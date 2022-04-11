45e787c21ad29b36be271e96ad5910b3d9d9aa5c
Jacob <hahkobeh@gmail.com>
2022-04-10 18:08:33 -0600

    item system

--- src/main/java/uofc/lifexp/itemsystem/inventory/InventoryRepository.java
+++ src/main/java/uofc/lifexp/itemsystem/inventory/InventoryRepository.java
@@ -3,6 +3,9 @@ package uofc.lifexp.itemsystem.inventory;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InventoryRepository extends MongoRepository<Inventory,String> {
+    List<Inventory> getAllByUsername(String username);
 }
