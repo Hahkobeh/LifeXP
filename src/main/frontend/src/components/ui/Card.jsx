022b6db129a08243e14a8be22a8dbb9c2b5e8eeb
colin <colin.christophe@ucalgary.ca>
2022-04-11 20:21:50 -0600

    Changing goal page layout

--- src/main/frontend/src/components/ui/Card.jsx
+++ src/main/frontend/src/components/ui/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {GiCancel , GiCheckMark} from 'react-icons/gi';
+import {GiBowlingPropulsion, GiCancel , GiCheckMark} from 'react-icons/gi';
 
 import './Card.scss';
 
@@ -10,55 +10,36 @@ function Card(props){
 
     const currentName = localStorage.getItem("username");
     async function completedHandler(){
-
-        //console.log(props.id);
-        //connect to database and change status of task to completed
         
         await axios.put(`http://localhost:8080/api/goal/complete/${props.id}`)
         props.reset();
     }
 
     async function cancelledHandler(){
-        //console.log(props.id);
-        //connect to database and change the task with props.id to cancelled
         
         await axios.delete(`http://localhost:8080/api/goal/delete/${props.id}`)
         props.reset();
     }
     if(props.status === 1){
-        return<div className={props.status === 2 ? "card overdue": "card"}>
-            <div className="card-cont">
-                <div className="goal-info">
-                    {props.children}
-                </div>
-            </div>
-
+        return<div className="card">
+            <h1 className = 'task-title-c'>{props.title}</h1>
 
         </div>
     }else{
         return<div className={props.status === 2 ? "card overdue": "card"}>
+            
+            <div className= 'goal-title'>
+                <h1 className = 'task-title'>{props.title}</h1>
+            </div>
 
-            <div className="card-cont">
-                <div className="goal-info">
-                    {props.children}
-                </div>
-                
-                
-                <div className="options">
-                    <li className="options-item">
-                        <button className='checkmark' onClick={completedHandler}>
-                            <GiCheckMark size={30} style={{color: 'green'}}/>
-                        </button>
-                    </li>
-                    <li className="options-item">
-                        <button className="x-mark" onClick={cancelledHandler}>
-                            <GiCancel size={30} style={{color: 'red'}}/>
-                        </button>
-                    </li>
-                    
+            <div className="options">
+                <button className='checkmark' onClick={completedHandler}>
+                    <GiCheckMark size={30} style={{color: 'green'}}/>
+                </button>
+                <button className="x-mark" onClick={cancelledHandler}>
+                    <GiCancel size={30} style={{color: 'red'}}/>
+                </button>
 
-                </div>
-                
             </div>
             
         </div>
